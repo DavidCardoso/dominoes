11b855798483425c92725e2c82d9d80f208030e5
David Cardoso <davidcardoso.ti@gmail.com>
2018-04-24 18:25:15 -0300

    WIP structure

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+	<meta name="description" content="Dominoes programming challenge.">
+	<meta name="keywords" content="PHP, Algorithm, Data Structures, Challenge, For Fun, Dominoes">
+	<meta name="author" content="David Cardoso Gomes da Silva">
+	
+	<title>Dominoes | David Cardoso</title>
+
+	<link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+	<script src="assets/js/script.js"></script>
+</head>
+<body>
+	<div class="content box-shadow">
+		<header class="">
+	 		<h1 class="app-icon">Dominoes</h1>
+	 		<hr>
+		</header>
+
+		<section class="">
+		 	<?php include_once 'view/board.html.php' ?>
+		</section>
+	</div>
+
+ 	<footer>
+ 		<div class="inline copyright"></div>
+ 		<div class="inline pipe"></div>
+ 		<div class="inline poweredby"><a href="https://davidcardoso.me">David Cardoso</a></div>
+	</footer>
+</body>
+</html>
