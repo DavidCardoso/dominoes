7d7238d418f7871a250399d1e4b090e5e12d9171
David Cardoso <davidcardoso.ti@gmail.com>
2018-04-24 22:53:46 -0300

    WIP Domino class

---
+++ view/board.html.php
@@ -0,0 +1,13 @@
+<?php 
+require_once '../class/Domino.class.php';
+
+$domino = new Domino();
+?>
+<h2>Board</h2>
+<div>
+	<?php foreach ($domino->getTiles() as $key => $value): ?>
+	<p>
+		<?php printf('Tile %s: <%s:%s>', ($key+1), $value[0], $value[1]); ?>
+	</p>
+	<?php endforeach; ?>
+</div>
