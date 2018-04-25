68496deeecbe97dbe5bf1e1548f0011df2ecb189
David Cardoso <davidcardoso.ti@gmail.com>
2018-04-25 03:58:19 -0300

    WIP playing... - Done: starting game - Done: make movements - Done: drawing from the stock - ToDo: display board status after every play - ToDo: check some situations where is drawing the same pile

--- view/board.html.php
+++ view/board.html.php
@@ -1,15 +1,33 @@
 <?php
-// ini_set( 'display_errors', '1' );
+ini_set( 'display_errors', '1' );
+ini_set( 'memory_limit', '256M' );
 require_once '../class/Domino.class.php';
 
-$domino = new Domino();
+$players = ['Celina', 'David'];
+$domino = new Domino($players);
 ?>
-<h2>Board</h2>
+<h2>Players</h2>
 <div>
-	<?php foreach ($domino->getTiles() as $key => $value): ?>
+	<?php foreach ($players as $key => $value): ?>
 	<p>
-		<?php printf('Tile %s: <%s:%s>', ($key+1), $value[0], $value[1]); ?>
+		<?php printf('%d) %s', $key+1, $value); ?>
 	</p>
 	<?php endforeach; ?>
+
 </div>
 
+<h3><?php echo $domino->startGame(); ?></h2>
+<div>
+	<?php 
+		while ( $domino->getWinner() === ''){
+			foreach ($players as $value) {
+				do {
+					list($played, $msg) = $domino->play($value);
+					echo '<p>'.$msg.'</p>';
+				} while (!$played);
+			}
+		}
+	?>
+</div>
+
+<h3><?php echo $domino->getWinner(); ?></h3>
